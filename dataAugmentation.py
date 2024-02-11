cb68620a167e02f5dcaa59ffc903bd5647b3e760
Indukuri Likhitha <likhithaindukuri07@gmail.com>
2024-02-11 14:52:02 +0530

    Final code

--- dataAugmentation.py
+++ dataAugmentation.py
@@ -4,83 +4,122 @@ import cv2
 import matplotlib.pyplot as plt
 import imutils
 
-class dataAugmenation:
+class DataAugmentation:
     def __init__(self):
         pass
-    def flip(self,num,datadir,targetdir):
+
+    def flip(self, num, datadir, targetdir):
+        """
+        Flip images horizontally or vertically.
+
+        Args:
+            num (int): 0 for flipping around the x-axis (vertical flip), positive value for flipping around the y-axis (horizontal flip).
+            datadir (str): Path to the directory containing the input images.
+            targetdir (str): Path to the directory where the flipped images will be saved.
+        """
         try:
-          if not os.path.exists(targetdir+"/flip"):
-            os.mkdir(targetdir+"/flip")
-        except Exception as err:
-            print("Error occurred while creating folder")
-        for image in list(os.listdir(datadir)):
-          img = cv2.imread(datadir+"/"+image)
-          if img is not None:
-            try:
-              flip = cv2.flip(img,num)
-              plt.imsave(targetdir+"/flip/flip-"+image,flip)
-            except Exception as e:
-              print("Error occured in Flipping")
-        return
-    def rotate(self,angle,datadir,targetdir):
-        try:
-          if not os.path.exists(targetdir+"/rotate"):
-            os.mkdir(targetdir+"/rotate")
+            if not os.path.exists(targetdir + "/flip"):
+                os.mkdir(targetdir + "/flip")
         except Exception as err:
             print("Error occurred while creating folder")
 
-        for image in list(os.listdir(datadir)):
-          img = cv2.imread(datadir+"/"+image)
-          if img is not None:
-            try:
-              rotated = imutils.rotate(img,angle)
-              plt.imsave(targetdir+"/rotate/rotate-"+image,rotated)
-            except Exception as e:
-              print("Error occured in Rotating")
+        for image in os.listdir(datadir):
+            img = cv2.imread(os.path.join(datadir, image))
+            if img is not None:
+                try:
+                    flip = cv2.flip(img, num)
+                    plt.imsave(os.path.join(targetdir, "flip", "flip-" + image), flip)
+                except Exception as e:
+                    print("Error occurred in flipping:", e, "for image:", image)
         return
-    def brightInc(self,datadir,targetdir):
+
+    def rotate(self, angle, datadir, targetdir):
+        """
+        Rotate images by a given angle.
+
+        Args:
+            angle (float): Angle by which the images will be rotated clockwise.
+            datadir (str): Path to the directory containing the input images.
+            targetdir (str): Path to the directory where the rotated images will be saved.
+        """
         try:
-          if not os.path.exists(targetdir+"/inbrighten"):
-            os.mkdir(targetdir+"/inbrighten")
+            if not os.path.exists(targetdir + "/rotate"):
+                os.mkdir(targetdir + "/rotate")
         except Exception as err:
             print("Error occurred while creating folder")
 
-        for image in list(os.listdir(datadir)):
-          img = cv2.imread(datadir+"/"+image)
-          if img is not None:
-            try:
-              bright = np.ones(img.shape,dtype= "uint8")*70
-              bright_add = cv2.add(img,bright)
-              plt.imsave(targetdir+"/inbrighten/inbrighten-"+image,bright_add)
-            except Exception as e:
-              print("Error occured in brighten")
+        for image in os.listdir(datadir):
+            img = cv2.imread(os.path.join(datadir, image))
+            if img is not None:
+                try:
+                    rotated = imutils.rotate(img, angle)
+                    plt.imsave(os.path.join(targetdir, "rotate", "rotate-" + image), rotated)
+                except Exception as e:
+                    print("Error occurred in rotating:", e, "for image:", image)
         return
-    def brightDec(self,datadir,targetdir):
+
+    def brightInc(self, datadir, targetdir):
+        """
+        Increase brightness of images.
+
+        Args:
+            datadir (str): Path to the directory containing the input images.
+            targetdir (str): Path to the directory where the brightened images will be saved.
+        """
         try:
-          if not os.path.exists(targetdir+"/debrighten"):
-            os.mkdir(targetdir+"/debrighten")
+            if not os.path.exists(targetdir + "/inbrighten"):
+                os.mkdir(targetdir + "/inbrighten")
         except Exception as err:
             print("Error occurred while creating folder")
 
-        for image in list(os.listdir(datadir)):
-          img = cv2.imread(datadir+"/"+image)
-          if img is not None:
-            try:
-              bright = np.ones(img.shape,dtype= "uint8")*70
-              bright_sub = cv2.subtract(img,bright)
-              plt.imsave(targetdir+"/debrighten/debrighten-"+image,bright_sub)
-            except Exception as e:
-              print("Error occured in brighten")
+        for image in os.listdir(datadir):
+            img = cv2.imread(os.path.join(datadir, image))
+            if img is not None:
+                try:
+                    bright = np.ones(img.shape, dtype="uint8") * 70
+                    bright_add = cv2.add(img, bright)
+                    plt.imsave(os.path.join(targetdir, "inbrighten", "inbrighten-" + image), bright_add)
+                except Exception as e:
+                    print("Error occurred in brightening:", e, "for image:", image)
         return
+
+    def brightDec(self, datadir, targetdir):
+        """
+        Decrease brightness of images.
+
+        Args:
+            datadir (str): Path to the directory containing the input images.
+            targetdir (str): Path to the directory where the darkened images will be saved.
+        """
+        try:
+            if not os.path.exists(targetdir + "/debrighten"):
+                os.mkdir(targetdir + "/debrighten")
+        except Exception as err:
+            print("Error occurred while creating folder")
+
+        for image in os.listdir(datadir):
+            img = cv2.imread(os.path.join(datadir, image))
+            if img is not None:
+                try:
+                    bright = np.ones(img.shape, dtype="uint8") * 70
+                    bright_sub = cv2.subtract(img, bright)
+                    plt.imsave(os.path.join(targetdir, "debrighten", "debrighten-" + image), bright_sub)
+                except Exception as e:
+                    print("Error occurred in darkening:", e, "for image:", image)
+        return
+
     def zoomin(self):
-      """To be done in Future"""
-      pass
+        """To be done in Future"""
+        pass
+
     def zoomout(self):
-      """To be done in Future"""
-      pass
+        """To be done in Future"""
+        pass
+
     def crop(self):
-      """To be done in Future"""
-      pass
+        """To be done in Future"""
+        pass
+
     def shear(self):
-      """To be done in Future"""
-      pass
+        """To be done in Future"""
+        pass
--- imageFilter.py
+++ imageFilter.py
@@ -4,66 +4,99 @@ import cv2
 import matplotlib.pyplot as plt
 import imutils
 
-class imageFilter:
+class ImageFilter:
     def __init__(self):
         pass
-    def median(self,datadir,targetdir):
+
+    def median(self, datadir, targetdir):
+        """
+        Apply median filter to images.
+
+        Args:
+            datadir (str): Path to the directory containing the input images.
+            targetdir (str): Path to the directory where the filtered images will be saved.
+        """
         try:
-          if not os.path.exists(targetdir+"/median"):
-            os.mkdir(targetdir+"/median")
-        except Exception as err:
-            print("Error occurred while creating folder")
-        for image in list(os.listdir(datadir)):
-          img = cv2.imread(datadir+"/"+image)
-          if img is not None:
-            try:
-              median = cv2.medianBlur(img, 5)
-              plt.imsave(targetdir+"/median/median-"+image, cv2.cvtColor(median, cv2.COLOR_RGB2BGR))
-            except Exception as e:
-              print("Error occured in Median filter")
-    def laplacian(self,datadir,targetdir):
-        try:
-          if not os.path.exists(targetdir+"/laplacian"):
-            os.mkdir(targetdir+"/laplacian")
-        except Exception as err:
-            print("Error occurred while creating folder")
-        for image in list(os.listdir(datadir)):
-          img = cv2.imread(datadir+"/"+image)
-          if img is not None:
-            try:
-              lap = cv2.Laplacian(img,cv2.CV_64F)
-              cv2.imwrite(targetdir+"/laplacian/laplacian-"+image,lap)
-            except Exception as e:
-              print("Error occured in laplacian filter")
-        return
-    def gaussian(self,datadir,targetdir):
-        try:
-          if not os.path.exists(targetdir+"/gaussian"):
-            os.mkdir(targetdir+"/gaussian")
-        except Exception as err:
-            print("Error occurred while creating folder")
-        for image in list(os.listdir(datadir)):
-          img = cv2.imread(datadir+"/"+image)
-          if img is not None:
-            try:
-              gb = cv2.GaussianBlur(img, (3, 3), 1, 1)
-              cv2.imwrite(targetdir+ "/gaussian/gaussian-" + image, gb)
-            except Exception as e:
-              print("Error occured in gaussian filter")
-        return
-    def bilateral(self,datadir,targetdir):
-        try:
-          if not os.path.exists(targetdir+"/bilateral"):
-            os.mkdir(targetdir+"/bilateral")
+            if not os.path.exists(os.path.join(targetdir, "median")):
+                os.mkdir(os.path.join(targetdir, "median"))
         except Exception as err:
             print("Error occurred while creating folder")
 
-        for image in list(os.listdir(datadir)):
-          img = cv2.imread(datadir+"/"+image)
-          if img is not None:
-            try:
-              bilateral = cv2.bilateralFilter(img, 9, 75, 75)
-              plt.imsave(targetdir+"/bilateral//bilateral-"+image,bilateral)
-            except Exception as e:
-              print("Error occured in bilateral filter")
-        return
+        for image in os.listdir(datadir):
+            img = cv2.imread(os.path.join(datadir, image))
+            if img is not None:
+                try:
+                    median = cv2.medianBlur(img, 5)
+                    plt.imsave(os.path.join(targetdir, "median", "median-" + image),
+                               cv2.cvtColor(median, cv2.COLOR_RGB2BGR))
+                except Exception as e:
+                    print("Error occurred in Median filter:", e, "for image:", image)
+
+    def laplacian(self, datadir, targetdir):
+        """
+        Apply Laplacian filter to images.
+
+        Args:
+            datadir (str): Path to the directory containing the input images.
+            targetdir (str): Path to the directory where the filtered images will be saved.
+        """
+        try:
+            if not os.path.exists(os.path.join(targetdir, "laplacian")):
+                os.mkdir(os.path.join(targetdir, "laplacian"))
+        except Exception as err:
+            print("Error occurred while creating folder")
+
+        for image in os.listdir(datadir):
+            img = cv2.imread(os.path.join(datadir, image))
+            if img is not None:
+                try:
+                    lap = cv2.Laplacian(img, cv2.CV_64F)
+                    cv2.imwrite(os.path.join(targetdir, "laplacian", "laplacian-" + image), lap)
+                except Exception as e:
+                    print("Error occurred in Laplacian filter:", e, "for image:", image)
+
+    def gaussian(self, datadir, targetdir):
+        """
+        Apply Gaussian filter to images.
+
+        Args:
+            datadir (str): Path to the directory containing the input images.
+            targetdir (str): Path to the directory where the filtered images will be saved.
+        """
+        try:
+            if not os.path.exists(os.path.join(targetdir, "gaussian")):
+                os.mkdir(os.path.join(targetdir, "gaussian"))
+        except Exception as err:
+            print("Error occurred while creating folder")
+
+        for image in os.listdir(datadir):
+            img = cv2.imread(os.path.join(datadir, image))
+            if img is not None:
+                try:
+                    gb = cv2.GaussianBlur(img, (3, 3), 1, 1)
+                    cv2.imwrite(os.path.join(targetdir, "gaussian", "gaussian-" + image), gb)
+                except Exception as e:
+                    print("Error occurred in Gaussian filter:", e, "for image:", image)
+
+    def bilateral(self, datadir, targetdir):
+        """
+        Apply bilateral filter to images.
+
+        Args:
+            datadir (str): Path to the directory containing the input images.
+            targetdir (str): Path to the directory where the filtered images will be saved.
+        """
+        try:
+            if not os.path.exists(os.path.join(targetdir, "bilateral")):
+                os.mkdir(os.path.join(targetdir, "bilateral"))
+        except Exception as err:
+            print("Error occurred while creating folder")
+
+        for image in os.listdir(datadir):
+            img = cv2.imread(os.path.join(datadir, image))
+            if img is not None:
+                try:
+                    bilateral = cv2.bilateralFilter(img, 9, 75, 75)
+                    plt.imsave(os.path.join(targetdir, "bilateral", "bilateral-" + image), bilateral)
+                except Exception as e:
+                    print("Error occurred in Bilateral filter:", e, "for image:", image)
